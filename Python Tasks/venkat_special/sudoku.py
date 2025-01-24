3b1391d13a92f8edfb1e786f2840502a078bc26e
Sriram0512 <sriramreddy0512@gmail.com>
2025-01-24 12:23:54 +0530

    new

--- Python Tasks/venkat_special/sudoku.py
+++ Python Tasks/venkat_special/sudoku.py
@@ -1,9 +1,11 @@
+#this function used to print the grid
 def matrix(arr):
     for i in range(9):
         for j in range(9):
             print(arr[i][j], end=" ")
         print()
 
+#this function is used to find 0's in the code
 def finding_spots(arr, l):
     for row in range(9):
         for col in range(9):
@@ -13,18 +15,21 @@ def finding_spots(arr, l):
                 return True
     return False
 
+#it checks wheather the number is already present in the row or not 
 def used_in_row(arr, row, num):
     for i in range(9):
         if (arr[row][i] == num):
             return True
     return False
 
+#it checks wheather the number is already present in the coloumn or not
 def used_in_col(arr, col, num):
     for i in range(9):
         if (arr[i][col] == num):
             return True
     return False
 
+#it checks wheather the number is already present in the 3x3 box or not
 def used_in_box(arr, row, col, num):
     for i in range(3):
         for j in range(3):
@@ -32,12 +37,14 @@ def used_in_box(arr, row, col, num):
                 return True
     return False
 
+#this function checks is it safe to place the number or not
 def check_location(arr, row, col, num):
     return (not used_in_row(arr, row, num) and
             not used_in_col(arr, col, num) and
             not used_in_box(arr, row, col, num))
 
 def solving_sudoku(arr):
+    #Main function to solve the Sudoku using backtracking
     l = [0, 0]
     if not finding_spots(arr, l):
         return True
@@ -45,9 +52,12 @@ def solving_sudoku(arr):
     row = l[0]
     col = l[1]
 
+    #trying to fix numbers in empty spots
     for num in range(1, 10):
         if check_location(arr, row, col, num):
             arr[row][col] = num
+            
+            #Recursively try to solve the rest of the puzzle
             if solving_sudoku(arr):
                 return True
             arr[row][col] = 0
